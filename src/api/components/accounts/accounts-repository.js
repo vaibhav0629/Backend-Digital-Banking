bba6f5cfd70c7a49cbfb2458368ea5095115a6a9
Denzel Mark Wahyudi <Denzelmacbook@Denzels-Macbook.local>
2026-04-16 10:37:37 +0700

    update getaccountby user id and added a new function

--- src/api/components/accounts/accounts-repository.js
+++ src/api/components/accounts/accounts-repository.js
@@ -8,8 +8,14 @@ async function getAccount(id) {
   return Accounts.findById(id);
 }
 
-async function getAccountByUserId(userId) {
-  return Accounts.findOne({ userId });
+async function getAccountByUserId(userId, accountType) {
+  return Accounts.findOne({ userId }, { accountType });
+}
+
+async function getAccountId(id) {
+  const account = await Accounts.findById(id);
+  // eslint-disable-next-line no-underscore-dangle
+  return account._id;
 }
 
 async function getBalance(id) {
@@ -29,4 +35,5 @@ module.exports = {
   getAccountByUserId,
   getBalance,
   setBalance,
+  getAccountId,
 };
